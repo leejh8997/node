b7b275a39586c78bd3d015cea974a752afeb2c74
leejh8997 <chelee24@naver.com>
2025-04-18 18:10:01 +0900

    2025.04.18

--- exam4.js
+++ exam4.js
@@ -7,6 +7,15 @@ setTimeout(()=>{
     console.log("test")
 },2000);
 
+// 4개 다 똑같은 기능
 let func1 = x => x;
-
+let func2 = (x) => {
+    return x;
+}
+let func3 = function(x){
+    return x;
+}
+function func4(x){
+    return x;
+}
 console.log(func1("hello"))
