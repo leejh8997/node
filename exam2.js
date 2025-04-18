51362b3724cf8f02b922ab7aaaa0b30c413aafbf
leejh8997 <chelee24@naver.com>
2025-04-18 18:09:39 +0900

    first commit

---
+++ exam2.js
@@ -0,0 +1,4 @@
+function test(){
+    console.log("hello js")
+}
+setTimeout(test,3000); // 함수, 시간
---
+++ exam4.js
@@ -0,0 +1,12 @@
+let i = 0;
+// setInterval(function(){
+//     console.log(i++)
+// }, 1000);
+
+setTimeout(()=>{
+    console.log("test")
+},2000);
+
+let func1 = x => x;
+
+console.log(func1("hello"))
